dac026edb0c0f8e1bd62597be07e516f11f0ec62
Ramon Geronimo <rgero215@gmail.com>
2019-05-31 13:17:57 -0400

    Users can submit and save posts

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const port = 3000
-
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const app = express();
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+// Set db
+require('./data/reddit-db');
+
 
 app.use('/static', express.static('public'))
 
@@ -12,7 +24,13 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new')
+})
+
 app.listen(port, () => {
     console.log(`The application is running in port ${port}`)
 });
+
+require('./controllers/posts.js')(app);
 
